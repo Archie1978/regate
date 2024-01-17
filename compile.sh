3b7fa5ae446b8038530ec052d0a997eedd57ccdf
Archie1978 <christophe.valmir@gmail.com>
2024-01-17 19:08:59 +0100

    Complete compile.sh exemple linux/windows under Unbuntu 20.04

--- compile.sh
+++ compile.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+#
+#
+#  Example compilation linux and windows
+#
+#
 
 cd webservice/www/regate/
 npm run build
+
+if [ $? -ne 0 ]; then
+echo "Package not found, install all package"
+npm install
+npm install xterm-addon-web-links
+npm run build
+if [ $? -ne 0 ]; then
+exit 1
+fi
+fi
+
 cd ../../..
 
 
 cd cmd/regate-standalone-user/
+
+if [ $1 -eq "win" ]; then
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build
+else
 go build 
+fi
